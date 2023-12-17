d833de0bd72d8784976b6433f519804c34f63d21
Bhanu <bhanu@krenovate.com>
2023-12-18 01:06:25 +0530

    feat: added nuxt/content project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-      '@nuxthq/studio'
-  ]
+      '@nuxthq/studio',
+      '@nuxt/content',
+  ],
+  content: {
+  }
 })
